fa8601dad2302cf46df5c30ed28cfd616b74f2aa
Elyochuer <jhosua.orrego@ucp.edu.co>
2023-11-09 17:38:39 -0500

    cuarto cambio

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>Mi mamol te amo</h1>
+      <h1>Meli lleve me a españa !!!!!</h1>
       
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
