c509c72712856e3fde5af0f32f57b05bd9d04978
Elyochuer <jhosua.orrego@ucp.edu.co>
2023-11-09 17:34:33 -0500

    tercer commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,10 +16,8 @@ function App() {
           <img src={reactLogo} className="logo react" alt="React logo" />
         </a>
       </div>
-      <h1>JHOSUA - YAIR</h1>
-      <h1>hala madrid</h1>
-      <h1>14 champions</h1>
-      <h1>Vite + React</h1>
+      <h1>Mi mamol te amo</h1>
+      
       <div className="card">
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
